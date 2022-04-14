6ff34a0b350d1cbd4d36c7ec9c5db8b08974aa0e
Collins Sirwani <sircollo71@gmail.com>
2022-04-14 15:53:26 +0300

    Add routes and routelinks in the navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GiphyMainComponent } from './giphy-main/giphy-main.component';
+import { RandomComponent } from './random/random.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'trending',component: GiphyMainComponent},
+  {path: 'random', component: RandomComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
