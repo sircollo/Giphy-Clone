d189073e5ec3a8381586fe7f57b156162c5ebd1f
Collins Sirwani <sircollo71@gmail.com>
2022-04-15 09:12:12 +0300

    Create routerlink to images

--- src/app/trending-service/trending.service.ts
+++ src/app/trending-service/trending.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 // import { images } from '../trending-gifs';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -10,24 +11,25 @@ export class TrendingService {
   gifs =new BehaviorSubject<any>([])
   gifNumber: number = 12;
   constructor(private http: HttpClient){}
-
+  trendingAP = environment.trendingAPI
   getTrendingGifs(gifNumber:any){
-    return this.http.get(`https://api.giphy.com/v1/gifs/trending?api_key=L5QeIMh6hYy1AV3zLvPEMtz8twlE3jbt&limit=12&rating=g`).subscribe((response:any)=>{
+    return this.http.get(`https://api.giphy.com/v1/gifs/trending?api_key=${environment.trendingAPI}&limit=12&rating=g`).subscribe((response:any)=>{
       this.gifs.next(response.data)
     })
+    // %$[environment.trendingAPI]
   }
   getMoreTrendingGifs(){
-    return this.http.get(`https://api.giphy.com/v1/gifs/trending?api_key=L5QeIMh6hYy1AV3zLvPEMtz8twlE3jbt&limit=100&rating=g`).subscribe((response:any)=>{
+    return this.http.get(`https://api.giphy.com/v1/gifs/trending?api_key=${environment.trendingAPI}&limit=100&rating=g`).subscribe((response:any)=>{
       this.gifs.next(response.data)
     })
   }
   getRandomGifs(){
-    return this.http.get(`https://api.giphy.com/v1/gifs/random?api_key=L5QeIMh6hYy1AV3zLvPEMtz8twlE3jbt&tag=sports&rating=g`).subscribe((response:any)=>{
+    return this.http.get(`https://api.giphy.com/v1/gifs/random?api_key=${environment.trendingAPI}&tag=sports&rating=g`).subscribe((response:any)=>{
       this.gifs.next(response.data)
     })
   }
   getMoreRandomGifs(){
-    return this.http.get(`https://api.giphy.com/v1/gifs/random?api_key=L5QeIMh6hYy1AV3zLvPEMtz8twlE3jbt&tag=sports&rating=g`).subscribe((response:any)=>{
+    return this.http.get(`https://api.giphy.com/v1/gifs/random?api_key=${environment.trendingAPI}&tag=sports&rating=g`).subscribe((response:any)=>{
       this.gifs.next(response.data)
     })
   }
@@ -35,7 +37,7 @@ export class TrendingService {
     return this.gifs.asObservable()
   }
   searchGifs(gifName:string){
-    return this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=L5QeIMh6hYy1AV3zLvPEMtz8twlE3jbt&q=laugh&limit=25&offset=0&rating=g&lang=en`).subscribe((response:any)=>{
+    return this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=${environment.trendingAPI}&q=laugh&limit=25&offset=0&rating=g&lang=en`).subscribe((response:any)=>{
       this.gifs.next(response.data)
     },err=>{
       this.gifs = new BehaviorSubject<any>("Try another Search Item")
